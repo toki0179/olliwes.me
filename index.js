265b1713a7bc3413b540fea7a887d7d848e114b3
toki0179 <olliwes01@gmail.com>
2022-01-25 20:26:23 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const path = require("path");
+const port = 3000
+
+// set view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/src/views"));
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+
+app.get("/projects", (req, res) => {
+    res.render("projects", { title: "Projects" })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' })
+})
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404, page not found !</h1>");
+});
+
+app.use((req, res, next) => {
+    res.status(500).send("<h1>500, A server error occured !</h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Server starting | express`);
+});
